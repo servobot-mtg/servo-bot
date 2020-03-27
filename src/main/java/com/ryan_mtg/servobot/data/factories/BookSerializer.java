814b504dfa62337ae089e44813df84bf1e280a72
ryan_mtg <servobot.mtg@gmail.com>
2020-03-27 12:25:40 -0500

    Fix BookTableEdit serialization to actually save the books

--- src/main/java/com/ryan_mtg/servobot/data/factories/BookSerializer.java
+++ src/main/java/com/ryan_mtg/servobot/data/factories/BookSerializer.java
@@ -63,6 +63,10 @@ public class BookSerializer {
     }
 
     public void commit(final int botHomeId, final BookTableEdit bookTableEdit) {
+        for (Book book : bookTableEdit.getSavedBooks()) {
+            saveBook(botHomeId, book);
+        }
+
         for (Statement statement : bookTableEdit.getDeletedStatements()) {
             statementRepository.deleteById(statement.getId());
         }
